70ab74fc2d0973890a3ead6dfcf6fc757199b1a5
Tomáš <46920928+WitchoutName@users.noreply.github.com>
2022-12-30 21:49:56 +0100

    playback speed UI

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sharepoint-video-speed
 Chrome extension for having the option to change the playback speed on Sharepoint video player
+
+| |  |
+| ----------- | ----------- |
+| ![alt text](images/showcase-icon.png) | ![alt text](images/showcase-options.png) |
---
+++ scripts/content.js
@@ -0,0 +1,91 @@
+const speedMenuHtml = `<div class="menu-base">
+<div class="menu-list">
+    </div>
+</div>`
+
+const speedItemHtml = (number)=>`<div class="menu-item">
+<div class="menu-item-icon"><svg fill="currentColor" class="speedTick" aria-hidden="true" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M14.05 3.49c.28.3.27.77-.04 1.06l-7.93 7.47A.85.85 0 014.9 12L2.22 9.28a.75.75 0 111.06-1.06l2.24 2.27 7.47-7.04a.75.75 0 011.06.04z" fill="currentColor"></path></svg></div>
+<div>${number}</div>
+</div>`
+
+const speedOptions = [
+    0.25,
+    0.5,
+    0.75,
+    1,
+    1.25,
+    1.5,
+    1.75,
+    2,
+    4,
+    8,
+    16
+]
+
+
+$(()=>{
+    let video = null;
+    let time = 0;
+    let menubarPlayItem;
+    let speedMenu;
+    const timeoutTime = 10 * 1000;
+
+    const main = ()=>{
+        const menubar = $("div.playback-experience-controls-container > div > div");
+        menubar.children()[2].after(document.createElement("div"));
+        menubarPlayItem = $(menubar.children()[3])
+        menubarPlayItem.attr("class", "menubar-item");
+        
+        menubarPlayItem.append("<button class='menubar-item-button' title='Speed'><img id='speed-icon' /></button>")
+        $("#speed-icon").attr("src", chrome.runtime.getURL("images/speed.png"))
+
+        menubarPlayItem.append(speedMenuHtml);
+        speedMenu = $(menubarPlayItem.children()[1])
+        speedMenu.hide();
+        // $("body").click()
+
+        speedOptions.forEach(o => {
+            speedMenu.append(speedItemHtml(o));
+            let item = $(speedMenu.children()[speedMenu.children().length-1])
+            
+            if(o != 1) item.find(".speedTick").hide();
+
+            item.click(()=>{
+                video[0].playbackRate = o;
+                $(".speedTick").hide();
+                item.find(".speedTick").show();
+            })
+        })
+
+        menubarPlayItem.on("click", ()=>{
+            speedMenu.toggle()
+        })
+
+        $(document).on("click", function(e) { 
+            if($(speedMenu).is(":visible" && e.currentTarget != menubarPlayItem)) {
+                speedMenu.hide()
+            }
+        });
+
+    }
+
+    const getVideoInterval = setInterval(function(){
+        if(time >= timeoutTime){
+            clearInterval(getVideoInterval);
+            return;
+        }
+
+        video = $('video');
+        if (video && video.length){
+            clearInterval(getVideoInterval);
+            video.on("play", function(){
+                video.unbind("play");
+                main();
+            })
+        }
+        
+        time += 200;
+    }, 200)
+
+
+})
