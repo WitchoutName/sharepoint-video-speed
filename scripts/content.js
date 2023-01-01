b7c403162ecc6a87ddaab5ce4be0c000eca8cfc3
Tomáš <46920928+WitchoutName@users.noreply.github.com>
2023-01-01 16:54:23 +0100

    search for the video element on link click

--- scripts/content.js
+++ scripts/content.js
@@ -17,75 +17,89 @@ const speedOptions = [
     1.5,
     1.75,
     2,
+    3,
     4,
     8,
     16
 ]
 
+const setupVideo = (video)=>{
+    const menubar = $("div.playback-experience-controls-container > div > div");
+    menubar.children()[2].after(document.createElement("div"));
+    menubarPlayItem = $(menubar.children()[3])
+    menubarPlayItem.attr("class", "menubar-item");
+    
+    menubarPlayItem.append("<button class='menubar-item-button' title='Speed'><img id='speed-icon' /></button>")
+    $("#speed-icon").attr("src", chrome.runtime.getURL("images/speed.png"))
+
+    menubarPlayItem.append(speedMenuHtml);
+    speedMenu = $(menubarPlayItem.children()[1])
+    speedMenu.hide();
+    // $("body").click()
+
+    speedOptions.forEach(o => {
+        speedMenu.append(speedItemHtml(o));
+        let item = $(speedMenu.children()[speedMenu.children().length-1])
+        
+        if(o != 1) item.find(".speedTick").hide();
+
+        item.click(()=>{
+            video[0].playbackRate = o;
+            $(".speedTick").hide();
+            item.find(".speedTick").show();
+        })
+    })
+
+    menubarPlayItem.on("click", ()=>{
+        speedMenu.toggle()
+    })
+
+    $(document).on("click", function(e) { 
+        if($(speedMenu).is(":visible" && e.currentTarget != menubarPlayItem)) {
+            speedMenu.hide()
+        }
+    });
+}
+
 
 $(()=>{
     let video = null;
+    let looking = false;
     let time = 0;
-    let menubarPlayItem;
-    let speedMenu;
-    const timeoutTime = 10 * 1000;
+    const timeoutTime = 10 * 10000;
 
-    const main = ()=>{
-        const menubar = $("div.playback-experience-controls-container > div > div");
-        menubar.children()[2].after(document.createElement("div"));
-        menubarPlayItem = $(menubar.children()[3])
-        menubarPlayItem.attr("class", "menubar-item");
-        
-        menubarPlayItem.append("<button class='menubar-item-button' title='Speed'><img id='speed-icon' /></button>")
-        $("#speed-icon").attr("src", chrome.runtime.getURL("images/speed.png"))
-
-        menubarPlayItem.append(speedMenuHtml);
-        speedMenu = $(menubarPlayItem.children()[1])
-        speedMenu.hide();
-        // $("body").click()
-
-        speedOptions.forEach(o => {
-            speedMenu.append(speedItemHtml(o));
-            let item = $(speedMenu.children()[speedMenu.children().length-1])
-            
-            if(o != 1) item.find(".speedTick").hide();
-
-            item.click(()=>{
-                video[0].playbackRate = o;
-                $(".speedTick").hide();
-                item.find(".speedTick").show();
-            })
-        })
-
-        menubarPlayItem.on("click", ()=>{
-            speedMenu.toggle()
-        })
-
-        $(document).on("click", function(e) { 
-            if($(speedMenu).is(":visible" && e.currentTarget != menubarPlayItem)) {
-                speedMenu.hide()
-            }
-        });
-
+    const findVideo = ()=>{
+        if(!looking){
+            looking = true;
+            console.log("looking for a video...", looking)
+            const getVideoInterval = setInterval(function(){
+                if(time >= timeoutTime){
+                    looking = false;
+                    console.log("timeout...", looking)
+                    clearInterval(getVideoInterval);
+                    return;
+                }
+                
+                video = $('video');
+                if (video && video.length){
+                    looking = false;
+                    console.log("video found", looking)
+                    clearInterval(getVideoInterval);
+                    video.on("play", function(){
+                        video.unbind("play");
+                        time = 0;
+                        setupVideo(video);
+                    })
+                }
+                
+                time += 200;
+            }, 200)
+        }
     }
-
-    const getVideoInterval = setInterval(function(){
-        if(time >= timeoutTime){
-            clearInterval(getVideoInterval);
-            return;
-        }
-
-        video = $('video');
-        if (video && video.length){
-            clearInterval(getVideoInterval);
-            video.on("play", function(){
-                video.unbind("play");
-                main();
-            })
-        }
+    console.log($("button[role='link']"))
+    $("button[role='link']").on("click", ()=>{findVideo()});
+    
+    findVideo();
         
-        time += 200;
-    }, 200)
-
-
+    
 })
